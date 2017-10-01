6b0ec485a66e27c381034ff23daad41677a5dbaf
Bjorn Neergaard <bjorn@neersighted.com>
2017-10-01 11:29:04 -0600

    update

--- js/formulau.js
+++ js/formulau.js
@@ -1,3 +1,3 @@
-(function($){"use strict";$('a.js-scroll-trigger').click(function(event){if(this.hash!===""){event.preventDefault()
+(function($){"use strict";$('a.js-scroll-trigger').click(function(event){if(this.hash!==""){event.preventDefault()
 window.scroll({top:$(this.hash).offset().top,left:0,behavior:'smooth'});window.location.hash=this.hash;}
 $('.navbar-collapse').collapse('hide');});$(window).scroll(function(){if($("#navbar").offset().top>100){$("#navbar").addClass("navbar-shrink");}else{$("#navbar").removeClass("navbar-shrink");}});})(jQuery);

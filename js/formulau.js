a7bebab98228ee7b6a35db6957e98a20587ff171
Bjorn Neergaard <bjorn@neersighted.com>
2017-10-01 11:26:48 -0600

    Simplify JS

--- js/formulau.js
+++ js/formulau.js
@@ -1,26 +1,21 @@
 (function($) {
   "use strict";
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+  $('a.js-scroll-trigger').click(function(event) {
+    if (this.hash !=== "") {
+      event.preventDefault()
+      window.scroll({ 
+        top: $(this.hash).offset().top,
+        left: 0,
+        behavior: 'smooth'
+      });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
+      window.location.hash = this.hash;
+    }
+
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Collapse the navbar when page is scrolled
   $(window).scroll(function() {
     if ($("#navbar").offset().top > 100) {
       $("#navbar").addClass("navbar-shrink");

fb09ed1b939e758c50ff7dd9d2bc6a116c86999f
Bjorn Neergaard <bjorn@neersighted.com>
2017-10-02 02:35:51 -0600

    More progress!

--- js/formulau.js
+++ js/formulau.js
@@ -2,28 +2,38 @@
   "use strict";
 
   // Smooth scrolling to hashes.
-  $('a.smoothscroll').on("click", function(event) {
-    if (this.hash !== "") {
+  $('a.smoothscroll').on('click', function(event) {
+    if (this.hash !== '') {
       event.preventDefault();
 
-      window.scroll({
-        top: $(this.hash).offset().top,
-        left: 0,
-        behavior: 'smooth'
-      });
+      var hash = this.hash;
+      var target = $(this.hash)
 
-      history.pushState(null, null, this.hash)
+      history.pushState(null, null, hash)
+
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 500, function () {
+        target.focus();
+
+        if (target.is(":focus")) {
+          return false;
+        } else {
+          target.attr('tabindex','-1');
+          target.focus();
+        }
+      });
     }
 
     $('.navbar-collapse').collapse('hide');
   });
 
-  // Shrink the navbar when we scroll 15% down.
-  $(window).on("load scroll", function() {
-    if ($("#navbar").offset().top > $(window).height() * 0.15) {
-      $("#navbar").addClass("navbar-shrink");
+  $(window).on('load scroll', function() {
+    // Shrink the navbar when we scroll 15% down.
+    if ($('#navbar').offset().top >= $(window).height() * 0.15) {
+      $('#navbar').addClass('navbar-shrink');
     } else {
-      $("#navbar").removeClass("navbar-shrink");
+      $('#navbar').removeClass('navbar-shrink');
     }
   });
 
